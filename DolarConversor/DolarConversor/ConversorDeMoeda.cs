fd77acbf10953461f5c55f18825bf244999dc62a
Guigous <gui_1810@hotmail.com>
2021-12-01 21:08:21 -0300

    Codigo Comentado

---
+++ DolarConversor/DolarConversor/ConversorDeMoeda.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolarConversor
+{
+    class ConversorDeMoeda
+    {
+        public double DolarCotacao { get; private set; }
+        public double Iof { get; private set; }
+        public double Dolares { get; private set; }
+
+        public ConversorDeMoeda(double dolares)
+        {
+            //cosntrutor
+            DolarCotacao = 5.61;
+            Iof = (4.08/100) + 1;
+            Dolares = dolares;
+        }
+
+        //Cotacao Dolar
+        //https://www.google.com/search?client=opera-gx&q=dolar+cotacao&sourceid=opera&ie=UTF-8&oe=UTF-8
+
+        //IOF
+        //https://www.istoedinheiro.com.br/aumento-do-iof-veja-onde-e-como-suas-contas-serao-afetadas/
+        //metodo de calculo do dolar
+        public double CalculoDolar()
+        {
+            double dolar = Dolares * DolarCotacao;
+            return dolar;
+        }
+        //metodo do calculo com IOF
+        public double CalculoDolarIOF()
+        {
+            double dolariof = CalculoDolar() * Iof;
+            return dolariof;
+
+        }
+        //Retorno do IOF
+        public string RetornoIOF()
+        {
+            double iof = (Iof * 100) - 1;
+            return Iof.ToString();
+        }
+    }
+}
---
+++ DolarConversor/DolarConversor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DolarConversor
+{
+          class Program
+          {
+            static void Main(string[] args)
+            {
+            //input de dados
+            Console.WriteLine("--------------- Conversor ---------------");
+            Console.WriteLine("Quantos dolares a serem comprados: ");
+            double dolares = double.Parse(Console.ReadLine());
+            
+
+            //instaciamento da clace
+            ConversorDeMoeda conversor = new ConversorDeMoeda(dolares);
+
+            //output de dados
+            Console.WriteLine("Cotacao Atual do Dolar: R$5.61 ");
+            Console.WriteLine("Dolares Comprados: " + dolares);
+            Console.WriteLine("Valor a ser Pago: " + conversor.CalculoDolar());
+            Console.WriteLine("Valor do IOF: 4.08%" );
+            Console.WriteLine("Valor total da compra com IOF: " + conversor.CalculoDolarIOF());
+
+
+
+
+
+
+
+
+        }
+          }
+    
+    
+}
